f7a844d41388fe18db80b0e03913f995f9bdd592
Future Star <futurestar425000>
2024-09-13 05:16:51 -0400

    init commit

---
+++ app/dashboard/nav-links.tsx
@@ -0,0 +1,15 @@
+'use client';
+ 
+import {
+  UserGroupIcon,
+  HomeIcon,
+  InboxIcon,
+} from '@heroicons/react/24/outline';
+import Link from 'next/link';
+import { usePathname } from 'next/navigation';
+ 
+// ...
+export default function NavLinks() {
+  const pathname = usePathname();
+  // ...
+}
